a8e5885df69c90895ebb87f0acd17108e606dc10
Quentin <Coulis1990@gmail.com>
2019-09-24 16:10:18 +0200

    Added arguments for debug

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import cv2
 import numpy as np
 from math import floor
 import tcod as libtcod
+import argparse
 
-DEBUG = True
+def parse_args():
+    """ Arguments parser. """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", help="Toggle debug mode", action='store_true')
+    args = parser.parse_args()
 
-def get_edges(img):
+    return args
+
+def get_edges(img, debug):
     # Convert to gray levels
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    if DEBUG:
+    if debug:
         cv2.imshow("B&W Image", img)
         cv2.waitKey(0)
 
@@ -16,31 +23,36 @@ def get_edges(img):
     # Otsu erases buildings, so manula thresholding for now
     thres, img =  cv2.threshold(img, 215, 255, cv2.THRESH_BINARY)
 
-    if DEBUG:
+    if debug:
         cv2.imshow("Binarised Image", img)
         cv2.waitKey(0)
 
     # Get edges
     edges = cv2.Canny(img,75,255)
 
-    if DEBUG:
+    if debug:
         cv2.imshow("Edges", edges)
         cv2.waitKey(0)
 
     # Morpho
     kernel = np.ones((2,2),np.uint8)
     dilate = cv2.dilate(edges, kernel, iterations = 1)
+
+    if debug:
+        cv2.imshow("Dilated", dilate)
+        cv2.waitKey(0)
+
     kernel = np.ones((3,3),np.uint8)
     erosion = cv2.dilate(dilate, kernel, iterations = 1)
 
-    if DEBUG:
-        cv2.imshow("Erode", erosion)
+    if debug:
+        cv2.imshow("Eroded", erosion)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
     return erosion
 
-def edges_to_ascii(edges):
+def edges_to_ascii(edges, debug):
     # map final size
     w = 150
     h = 150
@@ -58,7 +70,7 @@ def edges_to_ascii(edges):
             if np.count_nonzero(edges[x*br:(x*br)+br, y*bc:(y*bc)+bc]) > pixel_threshold:
                 ascii_map[y,x] = '#'
 
-    if DEBUG:
+    if debug:
         libtcod.console_set_custom_font('Anikki_square_8x8.png', libtcod.FONT_LAYOUT_ASCII_INROW)
         libtcod.console_init_root(w, h, 'Test', False)
         con = libtcod.console_new(w, h)
@@ -77,15 +89,16 @@ def edges_to_ascii(edges):
     return ascii_map
 
 
-def main():
+def main(args):
     # Load image
     img = cv2.imread('test.png')
-    img_edges = get_edges(img)
-    ascii_map = edges_to_ascii(img_edges)
+    img_edges = get_edges(img, args.debug)
+    ascii_map = edges_to_ascii(img_edges, args.debug)
 
 
 
 
 
 if __name__ == '__main__':
-    main()
+    args = parse_args()
+    main(args)
